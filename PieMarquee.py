55710b3b2b32fe74adc7aaa6856290f56d2580ce
rinalim <46272802+rinalim@users.noreply.github.com>
2019-04-11 22:44:02 +0900

    Create PieMarquee.py

---
+++ PieMarquee.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import os
+from subprocess import *
+from time import *
+
+def run_cmd(cmd):
+# runs whatever in the cmd variable
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    output = p.communicate()[0]
+    return output
+    
+while True:
+   romfile = "maintitle"
+   adv_romfile = run_cmd("ps -aux | grep advmame/bin/advmame | grep -v 'grep' | awk '{print $12}'")
+   if words[0] == "maintitle" and len(adv_romfile) <= 1: 
+       romfile = "maintitle"
+   else:
+       if len(adv_romfile) > 1 :
+           system = "mame-advmame"
+           romfile = adv_romfile.replace("\n","")
