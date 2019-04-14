6bc4d50016f943b2f962f3bc92b7d4e2d699f610
rinalim <46272802+rinalim@users.noreply.github.com>
2019-04-14 16:48:35 +0900

    Update convert.py

--- marquee/convert.py
+++ marquee/convert.py
@@ -1,1 +1,14 @@
-convert
+import sys, os
+from subprocess import *
+
+def run_cmd(cmd):
+# runs whatever in the cmd variable
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    output = p.communicate()[0]
+    return output
+  
+source_path = sys.argv[1]
+
+if os.path.isdir(source_path) == True:
+    file_list = os.listdir(source_path)
+    print file_list
